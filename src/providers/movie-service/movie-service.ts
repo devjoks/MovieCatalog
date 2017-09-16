d9f00eca3fe0b86b5df58c62e5da7c7410b0bb7d
Devjoks <devjoks@gmail.com>
2017-09-16 17:25:12 -0300

    Comentado Busqueda con http
    Se repiten algunas peliculas cuando consulta esta incompleta

--- src/providers/movie-service/movie-service.ts
+++ src/providers/movie-service/movie-service.ts
@@ -9,13 +9,12 @@ export class MovieServiceProvider {
   private listComicSearch:any[] = [];
   private _Comic:any[] = [];
   private listRecommendation:any[] = [];
-  private lastList:any[] = [];
+  // private lastList:any[] = [];
 
   cargo: any;
   private _APIKey:String = "00284bc66287b619fc67b99f83a88a02";
   private _URLBase: String = "https://api.themoviedb.org/3/";
-  // api_key="+ this._APIKey +"&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=true&page="+ index +"&year=2017
-  searchQuery: string = '';
+  // searchQuery: string = '';
   
   constructor(public http: Http) {
   
@@ -59,11 +58,11 @@ export class MovieServiceProvider {
     this._Comic["release_date"] = new Date(this._Comic["release_date"]);
   }
   private addMoreComic(_listComic:any[]){
-    if(!(JSON.stringify(this.lastList) === JSON.stringify(_listComic))){
+    // if(!(JSON.stringify(this.lastList) === JSON.stringify(_listComic))){
       this.listComic = this.listComic.concat(_listComic);
       this.listComicSearch = this.listComicSearch.concat(_listComic);
-      this.lastList = _listComic;
-    }
+    //   this.lastList = _listComic;
+    // }
   }
   private initializeItems() {
     this.listComicSearch = this.listComic;
@@ -80,20 +79,20 @@ export class MovieServiceProvider {
 
     if (val && val.trim() != '') {
       this.listComicSearch = this.listComicSearch.filter((item) => {
-        if(item.title.toLowerCase().indexOf(val.toLowerCase()) == -1){
-          this.loadQuery(val);
-        }
+        // if(item.title.toLowerCase().indexOf(val.toLowerCase()) == -1){
+        //   this.loadQuery(val);
+        // }
         return (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1 || item.release_date.toLowerCase().indexOf(val.toLowerCase()) > -1 );
       })
     }
   }
 
-  loadQuery(_query:String){
-    this.http.get(this._URLBase + "search/movie?api_key="+ this._APIKey + "&language=es-ES&page=1&query=" + _query)
-    .map( res => res.json())
-    .subscribe (data => {
-      this.addMoreComic(data.results);
-    });
+  // loadQuery(_query:String){
+  //   this.http.get(this._URLBase + "search/movie?api_key="+ this._APIKey + "&language=es-ES&page=1&query=" + _query)
+  //   .map( res => res.json())
+  //   .subscribe (data => {
+  //     this.addMoreComic(data.results);
+  //   });
 }
 
 }
